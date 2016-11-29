20b5e873dd5bfc37a705ff98d7269d38c324c7e9
John Vehikite <jdavet1244@gmail.com>
2016-11-29 07:33:43 -0800

    Group 7 Teach 12 Assignment; created YouTubeVideoAdapter.java which implements SocialMediaEntry; updated SocialMediaDemo.java to use YouTubevideoAdapter ojects, allowing analysis of YouTubeVideo objects

--- src/socialmedia/SocialMediaDemo.java
+++ src/socialmedia/SocialMediaDemo.java
@@ -63,7 +63,8 @@ public class SocialMediaDemo implements Runnable {
         
         YouTubeVideo video1 = new YouTubeVideo("MormonMessages", "Bullying",
                 "President Uchtdorf counsels us to stop bullying.");
-        YouTubeVideoAdapter ytva1 = new YouTubeVideoAdapter(video1);
+        // adapt YouTubeVideo object to be able to add to entries list
+		YouTubeVideoAdapter ytva1 = new YouTubeVideoAdapter(video1);
         
         YouTubeVideo video2 = new YouTubeVideo("MormonMessages", "Patterns of Light",
                 "Elder Bednar teaches about the Light of Christ.");
